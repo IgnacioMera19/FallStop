408592b1605da68bfc07add456363107b865abe1
Goldie3 <brunoaltamirano978@gmail.com>
2026-06-23 22:53:38 -0400

    arreglo de crasheo al cancelar el aviso

--- FallStop/BaseShake.cs
+++ FallStop/BaseShake.cs
@@ -6,11 +6,14 @@ namespace FallStop
     {
         private bool _warningAbierto = false;
 
+
+        // Maneja la aparición de la página y comienza a monitorear el acelerómetro
         protected override void OnAppearing()
         {
             base.OnAppearing();
             _warningAbierto = false;
 
+            // Si el acelerómetro no está siendo monitoreado, suscribirse al evento ShakeDetected y comenzar a monitorear
             try
             {
                 if (!Accelerometer.IsMonitoring)
@@ -25,8 +28,15 @@ namespace FallStop
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
+            // Si el acelerómetro está siendo monitoreado, dejar de monitorearlo
+            if (Accelerometer.IsMonitoring)
+            {
+                Accelerometer.Stop();
+                Accelerometer.ShakeDetected -= OnShakeDetected;
+            }
         }
 
+        // Maneja el evento de detección de sacudida
         private async void OnShakeDetected(object? sender, EventArgs e)
         {
             if (_warningAbierto) return;
@@ -36,6 +46,7 @@ namespace FallStop
             {
                 try
                 {
+                    // Muestra la ventana de advertencia
                     var warning = new WarningView();
                     warning.Disappearing += (s, args) => _warningAbierto = false;
                     await Navigation.PushModalAsync(warning);
