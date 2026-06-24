4cb5921504cfda22f5b1b4aa84dedcf21e0fc0fc
kumojustkumo-beep <kumojustkumo@gmail.com>
2026-06-24 15:49:13 -0400

    Add files via upload

--- FallStop/Views/InformationView.xaml.cs
+++ FallStop/Views/InformationView.xaml.cs
@@ -2,8 +2,13 @@ namespace FallStop.Views;
 
 public partial class InformationView : BaseShake
 {
-	public InformationView()
-	{
-		InitializeComponent();
-	}
+    public InformationView()
+    {
+        InitializeComponent();
+    }
+
+    public async void OnBackClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
